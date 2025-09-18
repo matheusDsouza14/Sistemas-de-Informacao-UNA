13b79176608ddc44344f25b310ab6e7915592c4a
Matheus D Souza <80216526+matheusDsouza14@users.noreply.github.com>
2025-09-18 16:24:13 -0300

    09182025-423

---
+++ Curso Java Foundations/secao2_2_Praticas/src/ProblemSet2_2.java
@@ -0,0 +1,10 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        //A cat
+        System.out.println("|\\---/|");
+        System.out.println("| o_o  |");
+        System.out.println("\\_^___/");
+    } 
+}
